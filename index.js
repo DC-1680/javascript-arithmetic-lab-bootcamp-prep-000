70100544bc6288161ab5ead979096bb999a04b36
Daryl Concha <concha.daryl@gmail.com>
2017-08-25 19:18:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ function add(a,b) {
   return (a+b);
 }
 
-function subtract(a,b) {
+function subtract(a, b) {
   return (a-b);
 }
 
-function multiply(a,b) {
+function multiply(a, b) {
   return (a*b);
 }
 
-function divide (a,b) {
-  return (a/b);
+function divide (a, b) {
+  return (a / );
 }
 function inc(a) {
   return (a++);
